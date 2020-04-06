6002305f6243a08d6f5cd34ce6ceb99fef558a38
Rares Matei <matei.rar@gmail.com>
2020-04-06 15:21:47 +0100

    update

--- apps/car-store/src/app/app.tsx
+++ apps/car-store/src/app/app.tsx
@@ -19,7 +19,7 @@ export const App = () => {
       </header>
       <main>
         <h2>Resources &amp; Tools</h2>
-        <p>Thank you for using and showing some ♥ for Nx10.</p>
+        <p>Thank you for using and showing some ♥ for Nx11.</p>
         <div className="flex github-star-container">
           <a
             href="https://github.com/nrwl/nx"

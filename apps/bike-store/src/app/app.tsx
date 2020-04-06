eff5d4e0d7218aea1f468bfe3ed2264f22f74721
Rares Matei <matei.rar@gmail.com>
2020-04-06 13:22:51 +0100

    update

--- apps/bike-store/src/app/app.tsx
+++ apps/bike-store/src/app/app.tsx
@@ -22,7 +22,7 @@ export const App = () => {
     <div className="app">
       <header className="flex">
         <Logo width="75" height="75" />
-        <h1>Welcome to bike-store!</h1>
+        <h1>Welcome to new bike-store!</h1>
       </header>
       <main>
         <p> The brand of the bike is: {mockBike.brand} </p>

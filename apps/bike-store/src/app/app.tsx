2e2aac2ae4e4477f58277ab483ef1b2d838772eb
Rares Matei <matei.rar@gmail.com>
2020-04-06 14:17:17 +0100

    update

--- apps/bike-store/src/app/app.tsx
+++ apps/bike-store/src/app/app.tsx
@@ -27,7 +27,7 @@ export const App = () => {
       <main>
         <p> The new brand of the bike is: {mockBike.brand} </p>
         <h2>Resources &amp; Tools</h2>
-        <p>Thank you for using and showing some ♥ for Nx.2</p>
+        <p>Thank you for using and showing some ♥ for Nx.3</p>
         <div className="flex github-star-container">
           <a
             href="https://github.com/nrwl/nx"
